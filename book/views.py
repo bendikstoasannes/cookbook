7c361eb1f1044ac5082972c81299e59696ebc23d
bendissa <skolebolla3@gmail.com>
2019-01-02 10:40:21 +0100

    Modify template to display recipes from database

--- book/views.py
+++ book/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.utils import timezone
 
 # Create your views here.
 
 def recipe_list(request):
-    return render(request, 'book/recipe_list.html', {})
+    recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'book/recipe_list.html', {'recipes': recipes})
