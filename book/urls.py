1951f83a11470419437d5911dcf7cac21fcc4449
bendissa <skolebolla3@gmail.com>
2019-01-03 13:00:14 +0100

    Add view and template for recipe detail page

--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recipe_list, name='recipe_list')
+    path('', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail')
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 from django.utils import timezone
 
@@ -7,3 +7,7 @@ from django.utils import timezone
 def recipe_list(request):
     recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'book/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'book/recipe_detail.html', {'recipe': recipe})
