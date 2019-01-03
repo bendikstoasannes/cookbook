2395665a288375cdcf8635259975c0c846dfaf29
bendissa <skolebolla3@gmail.com>
2019-01-03 14:59:12 +0100

    Add views to create/edit recipes

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
-    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail')
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/new', views.recipe_new, name='recipe_new'),
+    path('recipe/<int:pk>/edit', views.recipe_edit, name='recipe_edit')
 ]
--- book/views.py
+++ book/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Recipe
 from django.utils import timezone
+from .forms import RecipeForm
 
 # Create your views here.
 
 def recipe_list(request):
-    recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
     return render(request, 'book/recipe_list.html', {'recipes': recipes})
 
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'book/recipe_detail.html', {'recipe': recipe})
+
+def recipe_new(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.published_date = timezone.now()
+            recipe.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm()
+    return render(request, 'book/recipe_edit.html', {'form': form})
+
+def recipe_edit(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.published_date = timezone.now()
+            recipe.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'book/recipe_edit.html', {'form': form})
